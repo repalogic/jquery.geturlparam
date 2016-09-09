b54f9eea42b5003f543504b0c2de3d72a75d30ea
HApablazaG <hapablazag@gmail.com>
2016-09-09 17:15:07 -0300

    Fix some erros, add functionality
    - Fix some errors i found
    - Add the lastValue option to return only the last value found by default, like the $_GET from php work.

--- jquery.getUrlParam.js
+++ jquery.getUrlParam.js
@@ -10,63 +10,56 @@
  * 
  */
 jQuery.fn.extend({
-/**
-* Returns get parameters.
-*
-* If the desired param does not exist, null will be returned
-*
-* To get the document params:
-* @example value = $(document).getUrlParam("paramName");
-* 
-* To get the params of a html-attribut (uses src attribute)
-* @example value = $('#imgLink').getUrlParam("paramName");
-*/ 
- getUrlParam: function(strParamName){
-	  strParamName = escape(unescape(strParamName));
-	  
-	  var returnVal = new Array();
-	  var qString = null;
-	  
-	  if ($(this).attr("nodeName")=="#document") {
-	  	//document-handler
+	/**
+	*
+	* @param {boolean} lastValue - set to true will return only the last value found, false if you want all values. true by default.
+	*
+	* Returns get parameters.
+	*
+	* If the desired param does not exist, null will be returned
+	*
+	* To get the document params:
+	* @example value = $(document).getUrlParam("paramName");
+	* 
+	* To get the params of a html-attribut (uses src attribute)
+	* @example value = $('#imgLink').getUrlParam("paramName");
+	*/ 
+	getUrlParam: function(strParamName, lastValue = true){
+		strParamName = decodeURIComponent(strParamName);
 		
-		if (window.location.search.search(strParamName) > -1 ){
-			
-			qString = window.location.search.substr(1,window.location.search.length).split("&");
-		}
-			
-	  } else if ($(this).attr("src")!="undefined") {
-	  	
-	  	var strHref = $(this).attr("src")
-	  	if ( strHref.indexOf("?") > -1 ){
-	    	var strQueryString = strHref.substr(strHref.indexOf("?")+1);
-	  		qString = strQueryString.split("&");
-	  	}
-	  } else if ($(this).attr("href")!="undefined") {
-	  	
-	  	var strHref = $(this).attr("href")
-	  	if ( strHref.indexOf("?") > -1 ){
-	    	var strQueryString = strHref.substr(strHref.indexOf("?")+1);
-	  		qString = strQueryString.split("&");
-	  	}
-	  } else {
-	  	return null;
-	  }
-	  	
-	  
-	  if (qString==null) return null;
-	  
-	  
-	  for (var i=0;i<qString.length; i++){
-			if (escape(unescape(qString[i].split("=")[0])) == strParamName){
-				returnVal.push(qString[i].split("=")[1]);
+		var returnVal = new Array();
+		var qString = null;
+		
+		if ($(this).attr("nodeName") == "#document") {
+			//document-handler
+			if (window.location.search.search(strParamName) > -1 ){
+				qString = window.location.search.substr(1, window.location.search.length).split("&");
 			}
-			
-	  }
-	  
-	  
-	  if (returnVal.length==0) return null;
-	  else if (returnVal.length==1) return returnVal[0];
-	  else return returnVal;
+		} else if ($(this).attr("src") !== undefined) {
+			var strHref = $(this).attr("src");
+			if ( strHref.indexOf("?") > -1 ){
+				var strQueryString = strHref.substr(strHref.indexOf("?") + 1);
+				qString = strQueryString.split("&");
+			}
+		} else if ($(this).attr("href") !== undefined) {
+			var strHref = $(this).attr("href");
+			if ( strHref.indexOf("?") > -1 ){
+				var strQueryString = strHref.substr(strHref.indexOf("?") + 1);
+				qString = strQueryString.split("&");
+			}
+		} else {
+			return null;
+		}
+		
+		if (qString == null) return null;
+		
+		for (var i = 0; i < qString.length; i++){
+			var args = qString[i].split("=");
+			if (decodeURIComponent(args[0]) == strParamName){
+				returnVal.push(args[1] === undefined ? null : decodeURIComponent(args[1]));
+			}
+		}
+		
+		return returnVal.length == 0 ? null : returnVal.length == 1 ? returnVal[0] : lastValue ? returnVal[returnVal.length - 1] : returnVal;
 	}
 });
